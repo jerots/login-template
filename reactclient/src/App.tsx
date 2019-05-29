9edef03068a810f77277bc5d758da7905d8d57bd
ONG TEE SENG, JEREMY _ <jeremyong.2014@smu.edu.sg>
2019-05-29 23:05:32 +0800

    move profile methods to profile.tsx

--- reactclient/src/App.tsx
+++ reactclient/src/App.tsx
@@ -54,53 +54,10 @@ class App extends React.Component<Props, State> {
       },
       alerts: []
     };
-    this.getProfile = this.getProfile.bind(this);
     this.renderAlerts = this.renderAlerts.bind(this);
-    this.update = this.update.bind(this);
   }
 
-  async getProfile() {
-    const token = localStorage.getItem("access_token");
-    const response = await axios.get("http://localhost:5000/profile", {
-      headers: {
-        authorization: "Bearer " + token
-      }
-    });
-    if (response.status === 200) {
-      const profile = response.data;
-      return profile;
-    } else {
-      // handle others
-    }
-  }
-
-  async update(user: User) {
-    const token = localStorage.getItem("access_token");
-    const response = await axios.post(
-      "http://localhost:5000/update_profile",
-      {
-        email: user.email,
-        first_name: user.firstName,
-        last_name: user.lastName,
-        profile_picture_url: user.profilePictureURL
-      },
-      {
-        headers: {
-          authorization: "Bearer " + token
-        }
-      }
-    );
-    if (response.status === 200) {
-      this.setState({
-        alerts: [{ type: "success", message: "Profile updated successfully" }]
-      });
-    } else {
-      //TODO: handle others
-      this.setState({
-        alerts: [{ type: "danger", message: "Profile failed to update" }]
-      });
-    }
-  }
+  
 
   renderAlerts() {
     return (
@@ -119,7 +76,7 @@ class App extends React.Component<Props, State> {
       <div className="App" style={{ height: "100%" }}>
         {this.renderAlerts()}
         {this.props.authenticated ? (
-          <Profile update={this.update} getProfile={this.getProfile} />
+          <Profile />
         ) : (
           <Login />
         )}
--- reactclient/src/views/Profile.tsx
+++ reactclient/src/views/Profile.tsx
@@ -2,10 +2,9 @@ import React from "react";
 
 import { Form, Button } from "react-bootstrap";
 import { User } from "../App";
+import axios from "axios";
 
 type Props = {
-  update: (user: User) => void;
-  getProfile: () => any;
 };
 
 type State = {
@@ -26,10 +25,20 @@ class Profile extends React.Component<Props, State> {
     };
   }
 
-  async componentWillMount(){
-    const {first_name, last_name, email, profile_picture_url} = await this.props.getProfile();
+  async componentWillMount() {
+    const {
+      first_name,
+      last_name,
+      email,
+      profile_picture_url
+    } = await this.getProfile();
     const profilePictureURL = profile_picture_url || "";
-    this.setState({firstName: first_name, lastName: last_name, email, profilePictureURL});
+    this.setState({
+      firstName: first_name,
+      lastName: last_name,
+      email,
+      profilePictureURL
+    });
   }
 
   handleEmailChange(event: any) {
@@ -49,67 +58,112 @@ class Profile extends React.Component<Props, State> {
     this.setState({ lastName: newValue });
   }
 
-  handleProfilePictureURLChange(event:any){
+  handleProfilePictureURLChange(event: any) {
     const newValue = event.target.value;
 
     this.setState({ profilePictureURL: newValue });
   }
 
+  async getProfile() {
+    const token = localStorage.getItem("access_token");
+    const response = await axios.get("http://localhost:5000/profile", {
+      headers: {
+        authorization: "Bearer " + token
+      }
+    });
+    if (response.status === 200) {
+      const profile = response.data;
+      return profile;
+    } else {
+      // handle others
+    }
+  }
+
+  async update(user: User) {
+    const token = localStorage.getItem("access_token");
+    const response = await axios.post(
+      "http://localhost:5000/update_profile",
+      {
+        email: user.email,
+        first_name: user.firstName,
+        last_name: user.lastName,
+        profile_picture_url: user.profilePictureURL
+      },
+      {
+        headers: {
+          authorization: "Bearer " + token
+        }
+      }
+    );
+    if (response.status === 200) {
+      // this.setState({
+      //   alerts: [{ type: "success", message: "Profile updated successfully" }]
+      // });
+    } else {
+      //TODO: handle others
+      // this.setState({
+      //   alerts: [{ type: "danger", message: "Profile failed to update" }]
+      // });
+    }
+  }
+
   render() {
     return (
       <div style={styles.container}>
-          <Form style={styles.form}>
-            <Form.Group>
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="text"
-                value={this.state.email}
-                onChange={this.handleEmailChange.bind(this)}
-              />
-              <Form.Text className="text-muted" />
-            </Form.Group>
+        <Form style={styles.form}>
+          <Form.Group>
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="text"
+              value={this.state.email}
+              onChange={this.handleEmailChange.bind(this)}
+            />
+            <Form.Text className="text-muted" />
+          </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>First Name</Form.Label>
-              <Form.Control
-                type="text"
-                value={this.state.firstName}
-                onChange={this.handleFirstNameChange.bind(this)}
-              />
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control
-                type="text"
-                value={this.state.lastName}
-                onChange={this.handleLastNameChange.bind(this)}
-              />
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Profile Picture URL</Form.Label>
-              <Form.Control
-                type="text"
-                value={this.state.profilePictureURL}
-                onChange={this.handleProfilePictureURLChange.bind(this)}
-              />
-            </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+              type="text"
+              value={this.state.firstName}
+              onChange={this.handleFirstNameChange.bind(this)}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              type="text"
+              value={this.state.lastName}
+              onChange={this.handleLastNameChange.bind(this)}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Profile Picture URL</Form.Label>
+            <Form.Control
+              type="text"
+              value={this.state.profilePictureURL}
+              onChange={this.handleProfilePictureURLChange.bind(this)}
+            />
+          </Form.Group>
 
-            <Button
-              variant="primary"
-              type="submit"
-              onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-                event.preventDefault();
-                this.props.update({
-                  firstName: this.state.firstName,
-                  lastName: this.state.lastName,
-                  email: this.state.email,
-                  profilePictureURL: this.state.profilePictureURL
-                });
-              }}
-            >
-              Update
-            </Button>
-          </Form>
+          <Button
+            variant="primary"
+            type="submit"
+            onClick={(
+              event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+            ) => {
+              event.preventDefault();
+              this.update({
+                firstName: this.state.firstName,
+                lastName: this.state.lastName,
+                email: this.state.email,
+                profilePictureURL: this.state.profilePictureURL
+              });
+            }}
+          >
+            Update
+          </Button>
+        </Form>
       </div>
     );
   }
